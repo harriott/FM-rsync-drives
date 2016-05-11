fb1a2e44a7a42f4520c39f342401c70bee49bc9b
harriott <trohib@gmail.com>
2016-05-11 10:08:27 +0200

    better dialogue and exclude filter

--- rsync.sh
+++ rsync.sh
@@ -9,7 +9,63 @@
 #   and call it from a terminal with the bash command.
 #   eg: bash ~/Files/IT_stack/rsync-portabledrives/rsync.sh
 
-echo "This BASH script will run rsync, pushing all changes."
+# Prepare the locations:
+extmnt=/run/media/jo/
+mchn=sprbMb
+intdrv=/mnt/WD2000JD/
+if [ -d /mnt/BX200 ]; then
+	mchn=N130
+	intdrv=/mnt/BX200/
+fi
+intdir=( Dropbox/Close/ \
+         Dropbox/Copied/ \
+         Dropbox/F+F/ \
+         Dropbox/Further/ \
+         Dropbox/Now/ \
+         Dropbox/Photos/ \
+         Dropbox/Pointure_23/ \
+		 Dropbox/Stack/ \
+		 Files/ )
+# set to 0 to exclude a directory:
+include=( 1 \
+          1 \
+          1 \
+          1 \
+          1 \
+          1 \
+          1 \
+		  1 \
+		  1 )
+backupdir=( SAMSUNG/rsync-backup-$mchn/Dr_Close/ \
+            SAMSUNG/rsync-backup-$mchn/Dr_Copied/ \
+            SAMSUNG/rsync-backup-$mchn/Dr_F+F/ \
+            SAMSUNG/rsync-backup-$mchn/Dr_Further/ \
+            SAMSUNG/rsync-backup-$mchn/Dr_Now/ \
+			SAMSUNG/rsync-backup-$mchn/Dr_Photos/ \
+			SAMSUNG/rsync-backup-$mchn/Dr_Pointure_23/ \
+			SAMSUNG/rsync-backup-$mchn/Dr_Stack/ \
+			SAMSUNG/rsync-backup-$mchn/Files/ )
+extdrvdir=( K16GB500/Close/ \
+            SAMSUNG/Dr_Copied/ \
+            SAMSUNG/Dr_F+F/ \
+            K16GB500/Further/ \
+            K16GB500/Now/ \
+			SAMSUNG/Dr_Photos/ \
+            SAMSUNG/Dr_Pointure_23/ \
+			SAMSUNG/Dr_Stack/ \
+			SAMSUNG/Files/ )
+
+# List the included directories:
+echo -en "This BASH script will run \e[1mrsync\e[0m, pushing all changes, "
+echo "either to or from these local directories:"
+i=-1
+for thisdir in "${intdir[@]}"; do
+	((i++))
+	if [ ${include[i]} -ne "0" ]; then echo -en "\e[92m  $thisdir"; fi
+done
+echo -e "\e[0m"
+
+# Ask what to do:
 read -p "Sync the backup (b), or TO (t) portable drives, or FROM (f) (or simulate (n))?" drctn
 rsynccom="rsync -irtv --delete"
 if [ $drctn ]; then
@@ -32,41 +88,8 @@ if [ $drctn ]; then
 else
 	exit
 fi
-extmnt=/run/media/jo/
-mchn=sprbMb
-intdrv=/mnt/WD2000JD/
-if [ -d /mnt/BX200 ]; then
-	mchn=N130
-	intdrv=/mnt/BX200/
-fi
-backupdir=( SAMSUNG/rsync-backup-$mchn/Dr_Close/ \
-            SAMSUNG/rsync-backup-$mchn/Dr_Copied/ \
-            SAMSUNG/rsync-backup-$mchn/Dr_F+F/ \
-            SAMSUNG/rsync-backup-$mchn/Dr_Further/ \
-            SAMSUNG/rsync-backup-$mchn/Dr_Now/ \
-			SAMSUNG/rsync-backup-$mchn/Dr_Photos/ \
-			SAMSUNG/rsync-backup-$mchn/Dr_Pointure_23/ \
-			SAMSUNG/rsync-backup-$mchn/Dr_Stack/ \
-			SAMSUNG/rsync-backup-$mchn/Files/ )
-extdrvdir=( K16GB500/Close/ \
-            SAMSUNG/Dr_Copied/ \
-            SAMSUNG/Dr_F+F/ \
-            K16GB500/Further/ \
-            K16GB500/Now/ \
-			SAMSUNG/Dr_Photos/ \
-            SAMSUNG/Dr_Pointure_23/ \
-			SAMSUNG/Dr_Stack/ \
-			SAMSUNG/Files/ )
-# just prefix the following with a letter to exclude them:
-intdir=( iDropbox/Close/ \
-         iDropbox/Copied/ \
-         iDropbox/F+F/ \
-         iDropbox/Further/ \
-         iDropbox/Now/ \
-         iDropbox/Photos/ \
-         iDropbox/Pointure_23/ \
-		 iDropbox/Stack/ \
-		 iFiles/ )
+
+# Do it:
 i=-1
 outf=`basename ${BASH_SOURCE[0]}`
 outf="$intdrv${outf%.*}.txt"
@@ -74,29 +97,31 @@ echo "vim: tw=0:" > $outf
 echo "" | tee -a $outf
 echo $(date) | tee -a $outf
 for thisdir in "${intdir[@]}"; do
-	intlcn=$intdrv$thisdir
 	((i++))
-	if [ $drctn = "b" ]; then
-		fullcmd="$rsynccom $intlcn $extmnt${backupdir[i]}"
-	else
-		extdd=${extdrvdir[i]}
-		if [ ${extdd%%/*} = "K16GB500" ]; then
-			modrsc=" --modify-window=1"
-		else
-			modrsc=""
-		fi
-		if [ $drctn = "t" ]; then
-			fullcmd="$rsynccom$modrsc $intlcn $extmnt$extdd"
-		else
-			fullcmd="$rsynccom$modrsc $extmnt$extdd $intlcn"
-		fi
+	if [ ${include[i]} -ne "0" ]; then
+	    intlcn=$intdrv$thisdir
+	    if [ $drctn = "b" ]; then
+	    	fullcmd="$rsynccom $intlcn $extmnt${backupdir[i]}"
+	    else
+	    	extdd=${extdrvdir[i]}
+	    	if [ ${extdd%%/*} = "K16GB500" ]; then
+	    		modrsc=" --modify-window=1"
+	    	else
+	    		modrsc=""
+	    	fi
+	    	if [ $drctn = "t" ]; then
+	    		fullcmd="$rsynccom$modrsc $intlcn $extmnt$extdd"
+	    	else
+	    		fullcmd="$rsynccom$modrsc $extmnt$extdd $intlcn"
+	    	fi
+	    fi
+	    echo "" | tee -a $outf
+	    echo "Push sync $((i+1))" | tee -a $outf
+	    echo "-----------" | tee -a $outf
+	    echo $fullcmd | tee -a $outf
+	    echo "" | tee -a $outf
+	    $fullcmd | tee -a $outf # - disable for testing
 	fi
-	echo "" | tee -a $outf
-	echo "Push sync $((i+1))" | tee -a $outf
-	echo "-----------" | tee -a $outf
-	echo $fullcmd | tee -a $outf
-	echo "" | tee -a $outf
-	$fullcmd | tee -a $outf # - disable for testing
 done
 echo "- all done, and logged to $outf"
 exit
