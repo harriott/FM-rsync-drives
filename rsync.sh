4eb15f0711b7bdfd364243899116284773d94b4a
Joseph Harriott <trohib@gmail.com>
2020-07-03 06:20:32 +0200

    silent! /$underline

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim: set et tw=0:
 
-# Joseph Harriott http://momentary.eu/ Last updated: Mon 23 Dec 2019
+# Joseph Harriott http://momentary.eu/ Last updated: Sun 24 May 2020
 
 # A series of rsyncs between folders on local and portable media.
 # ---------------------------------------------------------------
@@ -11,25 +11,26 @@
 #   eg: bash /<fullpath>/rsync.sh
 
 # Prepare the locations for AcerVeritonT661:
-backuppath=/mnt/WD1001FALS/rsyncBackup-AVT661
 extmnt=/run/media/jo
 intdrv=/mnt/SDSSDA240G
 intdir=(
-  "/mnt/HD103SJ/Share-AV-Stack/" \
-  "/mnt/9QG2FFEE/Share-Dr-CAT-Buddhism/" \
-  "/mnt/9QG2FFEE/Share-Dr-CAT-Buddhism-Theravada/" \
-  "/mnt/9QG2FFEE/Share-Dr-CAT-OutThere/" \
-  "/mnt/9QG2FFEE/Share-Dr-CAT-OutThere-UK/" \
-  "/mnt/HD103SJ/Share-IT-Copied/" \
-  "/mnt/HD103SJ/Share-IT-DebianBased-Copied/" \
-  "/mnt/HD103SJ/Share-More/" \
-  "/mnt/HD103SJ/Share-toReduce/" \
+  "/mnt/9QG2FFEE/Share/AV-Stack/" \
+  "/mnt/9QG2FFEE/Share/Dr-CAT-Buddhism/" \
+  "/mnt/9QG2FFEE/Share/Dr-CAT-Buddhism-Theravada/" \
+  "/mnt/9QG2FFEE/Share/Dr-CAT-OutThere/" \
+  "/mnt/9QG2FFEE/Share/Dr-CAT-OutThere-UK/" \
+  "/mnt/9QG2FFEE/Share/IT-Copied/" \
+  "/mnt/9QG2FFEE/Share/IT-DebianBased-Copied/" \
+  "/mnt/9QG2FFEE/Share/More/" \
+  "/mnt/9QG2FFEE/Share/toReduce/" \
   "$intdrv/Dropbox/Apps/" \
   "$intdrv/Dropbox/Copied-OutThere/" \
   "$intdrv/Dropbox/JH/Cafezoide/" \
+  "$intdrv/Dropbox/JH/CforWork/" \
   "$intdrv/Dropbox/JH/Copied/" \
   "$intdrv/Dropbox/JH/F+F/" \
   "$intdrv/Dropbox/JH/IT_stack/" \
+  "$intdrv/Dropbox/JH/JCD-imagey-e3/" \
   "$intdrv/Dropbox/JH/Now/" \
   "$intdrv/Dropbox/JH/Theatre0/" \
   "$intdrv/Dropbox/JH/Theatre1/" \
@@ -38,7 +39,7 @@ intdir=(
   "$intdrv/Dropbox/JH/toReduce/" \
   "$intdrv/Dropbox/JH/Work/" \
   "$intdrv/Dropbox/Photos/" \
-  "$intdrv/Dropbox/CAM-fromSharon/" \
+  "$intdrv/Dropbox/CAM-toSort/" \
   "$intdrv/Dropbox/CAM-UK/" \
   "$intdrv/Dropbox/CAM-USA/" \
   "$intdrv/Dropbox/CAMusic/" \
@@ -46,36 +47,6 @@ intdir=(
 )
 scriptFolder=$( dirname "${BASH_SOURCE[0]}" )
 source "$scriptFolder/include.sh"
-backupdir=(
-  $backuppath/Share-AV-Stack/ \
-  $backuppath/Share-Dr-CAT-Buddhism/ \
-  $backuppath/Share-Dr-CAT-Buddhism-Theravada/ \
-  $backuppath/Share-Dr-CAT-OutThere/ \
-  $backuppath/Share-Dr-CAT-OutThere-UK/ \
-  $backuppath/Share-IT-Copied/ \
-  $backuppath/Share-IT-DebianBased-Copied/ \
-  $backuppath/Share-More/ \
-  $backuppath/Share-toReduce/ \
-  $backuppath/Sync1-Dr-Apps/ \
-  $backuppath/Sync0-Dr-Copied-OutThere/ \
-  $backuppath/Sync0-Dr-JH-Cafezoide/ \
-  $backuppath/Sync0-Dr-JH-Copied/ \
-  $backuppath/Sync0-Dr-JH-F+F/ \
-  $backuppath/Sync0-Dr-JH-IT_stack/ \
-  $backuppath/Sync0-Dr-JH-Now/ \
-  $backuppath/Sync0-Dr-JH-Theatre0/ \
-  $backuppath/Sync0-Dr-JH-Theatre1/ \
-  $backuppath/Sync0-Dr-JH-Then0/ \
-  $backuppath/Sync0-Dr-JH-Then1/ \
-  $backuppath/Sync0-Dr-JH-toReduce/ \
-  $backuppath/Sync0-Dr-JH-Work/ \
-  $backuppath/Sync0-Dr-Photos/ \
-  $backuppath/Sync1-Dr-CAM-fromSharon/ \
-  $backuppath/Sync1-Dr-CAM-UK/ \
-  $backuppath/Sync1-Dr-CAM-USA/ \
-  $backuppath/Sync1-Dr-CAMusic/ \
-  $backuppath/Sync1-Dr-JH-Stack/ \
-)
 # source "$scriptFolder/N130.sh"
 # This drive defines the sort order:
 extdrvdir=(
@@ -91,9 +62,11 @@ extdrvdir=(
   SM3/Sync1/Dr-Apps/ \
   SM3/Sync0/Dr-Copied-OutThere/ \
   SM3/Sync0/Dr-JH-Cafezoide/ \
+  SM3/Sync0/Dr-JH-CforWork/ \
   SM3/Sync0/Dr-JH-Copied/ \
   SM3/Sync0/Dr-JH-F+F/ \
   SM3/Sync0/Dr-JH-IT_stack/ \
+  SM3/Sync0/Dr-JH-JCD-imagey-e3/ \
   SM3/Sync0/Dr-JH-Now/ \
   SM3/Sync0/Dr-JH-Theatre0/ \
   SM3/Sync0/Dr-JH-Theatre1/ \
@@ -102,7 +75,7 @@ extdrvdir=(
   SM3/Sync0/Dr-JH-toReduce/ \
   SM3/Sync0/Dr-JH-Work/ \
   SM3/Sync0/Dr-Photos/ \
-  SM3/Sync1/Dr-CAM-fromSharon/ \
+  SM3/Sync1/Dr-CAM-toSort/ \
   SM3/Sync1/Dr-CAM-UK/ \
   SM3/Sync1/Dr-CAM-USA/ \
   SM3/Sync1/Dr-CAMusic/ \
@@ -123,17 +96,14 @@ for thisdir in "${intdir[@]}"; do
         fi
     fi
 done
-printf -v included "%02d" $j
+printf -v included "%02d" $j  # catches the total number of included locations
 echo -e "\e[0m"
 
 # Ask what to do:
-read -p "Sync the backup (b), or TO (T) portable drives (or simulate (t)), or FROM (F) (or simulate (f))? " drctn
+read -p "Sync TO (T) portable drives (or simulate (t)), or FROM (F) (or simulate (f))? " drctn
 rsynccom="rsync -ilrtv --delete"
 if [ $drctn ]; then
-    if [ $drctn = "b" ]; then
-        echo -e "Okay, running: \e[1m$rsynccom <localdrive> <backupdrivebackup>\e[0m"
-        cnfrm="y"
-    elif [ $drctn = "T" ]; then
+    if [ $drctn = "T" ]; then
         read -p "Run several: $rsynccom <localdrive> <portabledrive> ? " cnfrm
     elif [ $drctn = "t" ]; then
         rsynccom="rsync -ilnrtv --delete" # simulate
@@ -162,34 +132,38 @@ outf1="$outf0.log"
 echo "vim: tw=0:" > $outf1
 echo "" | tee -a $outf1
 echo $(date) | tee -a $outf1
+underline='------------------'
 j=0
 for thisdir in "${intdir[@]}"; do
     ((i++))
     if [ ${include[i]} -ne "0" ]; then
         if [ ! $thisdir = "-" ]; then
             ((j++))
-            if [ $drctn = "b" ]; then
-                fullcmd="$rsynccom $thisdir ${backupdir[i]}"
+            extdd=${extdrvdir[i]}
+            if [ ${extdd%%/*} = 'K16GB500' ] # it's a FAT32 drive
+            then
+                modrsc=" --modify-window=1" # don't send fraction of second changes
             else
-                extdd=${extdrvdir[i]}
-                if [ ${extdd%%/*} = "K16GB500" ]; then
-                    modrsc=" --modify-window=1" # don't send fraction of second changes
-                else
-                    modrsc=""
-                fi
-                if [ ${drctn,,} = "t" ]; # case-insensitive test
-                then
-                    fullcmd="$rsynccom$modrsc $thisdir $extmnt/$extdd"
-                else
-                    modrsc=" --modify-window=1" # don't receive fraction of second changes
-                    fullcmd="$rsynccom$modrsc $extmnt/$extdd $thisdir"
-                fi
+                modrsc=""
+            fi
+            if [ ${drctn,,} = "t" ]; # case-insensitive test
+            then
+                cmd="$rsynccom$modrsc"
+                fr="$thisdir"
+                to="$extmnt/$extdd"
+            else
+                modrsc=" --modify-window=1" # don't receive fraction of second changes
+                cmd="$rsynccom$modrsc"
+                fr="$extmnt/$extdd"
+                to="$thisdir"
             fi
             echo "" | tee -a $outf1
             printf -v include "%02d" $j
             echo "Push sync $include of $included" | tee -a $outf1
-            echo "------------------" | tee -a $outf1
+            echo $underline | tee -a $outf1
+            fullcmd="$cmd $fr $to"
             echo $fullcmd | tee -a $outf1
+            [ ! -d $fr ] && echo "--- !!! $fr ain't there !!! ---" >> $outf1
             echo "" | tee -a $outf1
             $fullcmd | tee -a $outf1 # - disable for testing
         fi
@@ -199,5 +173,6 @@ jHM=$(date "+%j-%H%M")
 outf2="$outf0-$jHM.log"
 cp $outf1 $outf2
 echo "- all done, and logged to $outf1 (& $outf2)"
+gvim -c "silent! /$underline" $outf1
 exit
 
---
+++ rync-Dr0.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# Joseph Harriott
+
+# A series of rsyncs between folders on local and portable archive media.
+# alias rs0 is defined in my bashrc
+
+ac="rsync -irtv --delete --progress $DROPBOX"
+
+  al='/run/media/jo/HDT721016SLA380/k6i-Dr0'
+# al='/run/media/jo/HM250HI/k3q-Dr0'
+# al='/run/media/jo/ST3320418AS/jcn-Dr0'
+# al='/run/media/jo/ST380815AS/k4n-Dr0'
+# al='/run/media/jo/WD1600AAJS/k46-Dr0'
+# al='/run/media/jo/WD2000JD/k59-Dr0'
+
+$ac/Copied-OutThere/ $al-Copied-OutThere
+$ac/JH/Cafezoide/    $al-JH-Cafezoide
+$ac/JH/CforWork/     $al-JH-CforWork
+$ac/JH/Copied/       $al-JH-Copied
+$ac/JH/F+F/          $al-JH-F+F
+$ac/JH/IT_stack/     $al-JH-IT_stack
+$ac/JH/Now/          $al-JH-Now
+$ac/JH/Stack/        $al-JH-Stack
+$ac/JH/Theatre0/     $al-JH-Theatre0
+$ac/JH/Theatre1/     $al-JH-Theatre1
+$ac/JH/Then0/        $al-JH-Then0
+$ac/JH/Then1/        $al-JH-Then1
+$ac/JH/toReduce/     $al-JH-toReduce
+$ac/JH/Work/         $al-JH-Work
+$ac/Photos/          $al-Photos
+
